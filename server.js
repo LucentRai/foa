18cf86cdec0967218450181eb26c5799787da086
Lucent Rai <lucentthulung@gmail.com>
2023-05-05 22:57:20 +0545

    server created

--- server.js
+++ server.js
@@ -7,5 +7,23 @@ const app = require('./app');
 
 mongoose
 	.connect(process.env.LOCAL_DB)
-	.then( _ => console.log('DATABASE CONNECTED'));
+	.then( () => console.log('DATABASE CONNECTED'));
 
+const server = app.listen(process.env.SERVER_PORT, () => {
+	console.log(`Server running on port ${process.env.SERVER_PORT}`);
+});
+
+process.on('uncaughtException', err => {
+	console.log(err.name, err.message);
+	console.log('Unhandled Exception: Shutting Down...');
+	process.exit(1);
+});
+
+process.on('unhandledRejection', err => {
+	console.log(err.name, err.message);
+	console.log('Unhandled Rejection: Shutting Down...');
+
+	server.close(() => {
+		process.exit(1);
+	});
+});
