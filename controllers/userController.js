d907caff2bb358a023477663bf56ff68d1ca9a9a
Lucent Rai <lucentthulung@gmail.com>
2023-05-21 09:32:14 +0545

    getMyProfile added

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,13 @@ const AppError = require('../utils/AppError');
 const User = require('../models/UserModel');
 const factoryFunc = require('./handlerFactory');
 
+async function getMyProfile(req, res, next){
+	res.status(200)
+		.json({
+			data: req.userInfo
+		})
+}
+
 async function updateMe(req, res, next){
 	if(req.body.password){
 		return new AppError(`To update password, use /updatePassword path`, 400);
@@ -10,6 +17,7 @@ async function updateMe(req, res, next){
 }
 
 module.exports = {
+	getMyProfile,
 	getUser: factoryFunc.getOne(User),
 	getAllUsers: factoryFunc.getAll(User),
 	createUser: factoryFunc.createOne(User),
