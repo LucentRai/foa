e2709ee72df699bb6b744554bcebd6e7e3eaba06
Lucent Rai <lucentthulung@gmail.com>
2023-05-30 11:49:41 +0545

    Schema modified

--- models/BranchModel.js
+++ models/BranchModel.js
@@ -3,19 +3,18 @@ const mongoose = require('mongoose');
 const branchSchema = new mongoose.Schema({
 	name: {
 		type: String,
+		unique: [true, 'Branch name must be unique'],
 		required: [true, 'Branch name required'],
 		trim: true,
 		maxlength: [20, 'Branch name must not exceed 20 characters']
 	},
 	workers: {
 		type: [mongoose.Schema.ObjectId],
-		ref: 'User',
-		required: [true, 'At least one employee is required']
+		ref: 'User'
 	},
 	menu: {
 		type: [mongoose.Schema.ObjectId],
-		ref: 'Food',
-		required: [true, 'At least one food item should be provided']
+		ref: 'Food'
 	},
 	ratingsAverage: {
 		type: Number,
@@ -24,9 +23,27 @@ const branchSchema = new mongoose.Schema({
 	ratingsQuantity: {
 		type: Number,
 		default: 0
-	}
+	},
+	address: {
+		type: String,
+		trim: true,
+		required: [true, 'Branch must have adddress'],
+		maxlength: [100, 'Branch location cannot exceed 100 characters']
+	},
+	slug: String
 });
 
+
 const BranchModel = mongoose.model('Branch', branchSchema, 'branches');
+
+// INDICES
+branchSchema.index({slug: 1});
+
+/****************************** MIDDLEWARES ******************************/
+branchSchema.pre(/^find/, function(next){
+	this.select('-__v');
+	console.log('h')
+	next();
+});
 
 module.exports = BranchModel;
