7ec003151319e80194effb6a59c8e1f97a8858d8
Lucent Rai <lucentthulung@gmail.com>
2023-06-02 22:05:02 +0545

    create menu functionality added

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,8 +1,8 @@
 const Food = require('../models/FoodModel');
+const Canteen = require('../models/CanteenModel');
 const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 const factoryFunc = require('./handlerFactory');
-const Canteen = require('../models/CanteenModel');
 
 async function getCanteenMenu(req, res, next){
 	const canteenId = await Canteen.findOne({slug: req.params.canteenSlug}).select('id');
@@ -10,7 +10,10 @@ async function getCanteenMenu(req, res, next){
 }
 
 async function createMenu(req, res, next){
-	
+	if(req.userInfo.role === 'canteen'){
+		req.body.canteen = req.userInfo.canteen;
+	}
+	factoryFunc.createOne(Food)(req, res, next);
 }
 
 async function updateMyMenu(req, res, next){
